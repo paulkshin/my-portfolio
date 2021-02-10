a208d0336319d967ccd866706ebb5fa8703452a8
Paul Shin <paulkisooshin@gmail.com>
2021-02-09 19:47:35 -0500

    First Commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,12 @@ function Layout({ children }) {
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
       <Header />
 
-      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
+      <main className="flex-1 w-full max-w-7xl px-4 py-8 mx-auto md:px-8 md:py-16">
         {children}
       </main>
 
       <footer className="bg-blue-700">
-        <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
+        <nav className="flex justify-between max-w-7xl p-4 mx-auto text-sm md:p-8">
           <p className="text-white">
             Created by{` `}
             <a
@@ -22,7 +22,7 @@ function Layout({ children }) {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Taylor Bryant
+              Paul Shin
             </a>
           </p>
 
