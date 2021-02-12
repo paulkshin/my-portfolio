e477fcbe3971ce6685edc39f878544f4f9eabda0
Paul Shin <paulkisooshin@gmail.com>
2021-02-11 22:35:07 -0500

    added typwriter npm yeet

---
+++ src/components/Typewriter.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import TypeWriterEffect from "react-typewriter-effect";
+
+export default function Typewriter() {
+  return (
+    <TypeWriterEffect
+      textStyle={{
+        fontFamily: "Red Hat Display",
+        color: "#3F3D56",
+        fontWeight: 500,
+        fontSize: "1.5em",
+      }}
+      startDelay={2000}
+      cursorColor="#3F3D56"
+      multiText={[
+        "Hey there, This is a type writer animation package",
+        "it consist of two types...",
+        "Single text display and multi text display",
+        "Fonts can be customized.",
+        "The type speed can be customized as well",
+      ]}
+      multiTextDelay={1000}
+      typeSpeed={30}
+    />
+  );
+}
