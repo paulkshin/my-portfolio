ed6ae0a4739a27a69d82d5cbacfc98237d998cb5
Paul Shin <paulkisooshin@gmail.com>
2021-02-11 22:28:25 -0500

    file created

---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <section className="h-screen w-screen">
+      <div className="flex">
+        <div>Hello</div>
+        <div>World</div>
+      </div>
+    </section>
+  );
+}
